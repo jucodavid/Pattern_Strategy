dd9bec1b1b3cb7d0493d7f32aa5b0164489d44aa
jucod <jucod@ventilator.lan>
2020-06-21 15:07:08 +0200

    ...

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,26 @@
 import comportement.*;
 
 class Test{
+
   public static void main(String[] args) {
     Personnage pers = new Guerrier();
-    pers.soigner();		
+    pers.soigner();     
     pers.setSoin(new Operation());
-    pers.soigner();		
+    pers.soigner();
+    
+    //Utilisation d'une classe anonyme
+    pers.setSoin(new Soin() {
+		public void soigne() {
+			System.out.println("Je soigne avec une classe anonyme ! ");
+		}
+
+		@Override
+		public void soigner() {
+			// TODO Auto-generated method stub
+			
+		}    	
+    });
+    
+    pers.soigner();
   }
 }
